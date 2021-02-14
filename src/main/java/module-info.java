674515670a98c42837ac4e63c59197e79b9f7f53
DMan <dimentr.2013@yandex.ru>
2021-02-14 19:43:48 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,8 @@ module BinanceFuturesSDK {
     requires org.apache.commons.codec;
 
     exports com.binance.client;
+    exports com.binance.client.constant;
+    exports com.binance.client.exception;
+    exports com.binance.client.impl;
+    exports com.binance.client.model;
 }
